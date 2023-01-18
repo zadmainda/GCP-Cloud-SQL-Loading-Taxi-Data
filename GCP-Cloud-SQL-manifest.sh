bf6c62e063c0560651f0575cab3cbe9bbd3798cc
Zadock Mainda <kinara.zadock@gmail.com>
2023-01-18 23:33:32 +0300

    feat: add Cloud instance root access password and retieve IP adress

--- GCP-Cloud-SQL-manifest.sh
+++ GCP-Cloud-SQL-manifest.sh
@@ -13,3 +13,14 @@ export BUCKET=${PROJECT_ID}-ml
 #Create Cloud SQL instance from CLI
 gcloud sql instances create taxi \
     --tier=db-n1-standard-1 --activation-policy=ALWAYS
+
+#set a password for the CLoud SQL instance root access
+gcloud sql users set-password root --host % --instance taxi \
+ --password Passw0rd
+
+#retrieve the cloud shell IP adress
+export ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
+
+#Whitelist Cloud Instance 
+gcloud sql instances patch taxi --authorized-networks $ADDRESS
+
