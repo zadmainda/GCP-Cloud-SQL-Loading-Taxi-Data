f90c1e3253205438d01eb417b02c3f4438a4f18b
Zadock Mainda <kinara.zadock@gmail.com>
2023-01-18 23:28:11 +0300

    feat: create a cloud sql instance

--- GCP-Cloud-SQL-manifest.sh
+++ GCP-Cloud-SQL-manifest.sh
@@ -9,3 +9,7 @@ gcloud config list project
 #intialize environment variables 
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
 export BUCKET=${PROJECT_ID}-ml
+
+#Create Cloud SQL instance from CLI
+gcloud sql instances create taxi \
+    --tier=db-n1-standard-1 --activation-policy=ALWAYS
